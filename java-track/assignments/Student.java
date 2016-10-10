adec749a1280c22cf79b9de417be5ae652d78eb2
SLaguerta <slaguerta@alumni.nd.edu>
2016-10-10 15:17:45 -0500

    completed Student class

--- java-track/assignments/Student.java
+++ java-track/assignments/Student.java
@@ -1,13 +1,9 @@
 
 public class Student {
 	private String firstName;
-	
 	private String lastName;
-	
 	private int studentID;
-	
 	private double GPA;
-	
 	private int credits;
 	
 	public Student (String firstName, String lastName, int studentID){
@@ -18,13 +14,22 @@ public class Student {
 		
 	}
 	
-	public Student studentInit (String firstName, String lastName, int studentID, int credits, double GPA){
+	public void studentInit(String firstName, String lastName, int studentID, int credits, double GPA){
 		
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.studentID = studentID;
 		this.credits = credits;
 		this.GPA = GPA;
+	}
+
+	public Student(String firstName, String lastName, int studentID, int credits, double GPA){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.studentID = studentID;
+		this.credits = credits;
+		this.GPA = GPA;
+	
 	}
 	
 	public String getName(){
@@ -48,23 +53,48 @@ public class Student {
 	}
 	
 	public String getClassStanding(){
-		if (this.credits <= 30){
+		if (this.credits < 30){
 			return "Freshman";
-		}else if (this.credits <= 60){
+		}else if (this.credits < 60){
 			return "Sophomore";
-		}else if (this.getCredits() <= 90){
+		}else if (this.credits < 90){
 			return "Junior";
 		}else return "Senior";
 		
 	}
 	
-	public double submitGrade(int credits, double gpatotal){
+	public void submitGrade( double courseGrade, int courseCredits){
 		//quality score= num of credits for a course*grade of a course 
 		//newgpa = sum of all quality scores divided by total number of credits
+		double qualityScore = courseGrade*courseCredits;
+		double qualitySubtotal = this.GPA*this.credits;
+		this.credits += courseCredits;
+		qualitySubtotal += qualityScore;
+		
+		
+		double newGPA = qualitySubtotal/this.credits;
+		
+		this.GPA = (double) Math.round(newGPA*1000.0)/1000.0;
+		
 	}
-	
-	
-	
+		
+	public double computeTuition(){
+		//leftover after 15 credits
+		double perCredit = (this.credits%15)*1333.33;
+		//for every 15 credits
+		double perSemester = (this.credits/15)*20000;
+		double tuition = perCredit + perSemester;
+		return tuition;
+	}
+		
+	public Student createLegacy(Student parentOne, Student parentTwo){
+		return new Student(
+				parentOne.getName(),
+				parentTwo.getName(),
+				parentOne.getStudentID() + parentTwo.getStudentID(),
+				Math.max(parentOne.getCredits(), parentTwo.getCredits()),
+				(parentOne.getGPA() + parentTwo.getGPA())/2 );
+	}
 	
 	public String toString(){
 		return "Name: " + this.firstName + " " + this.lastName + ", " + this.studentID;
