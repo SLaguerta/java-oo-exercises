c905232bbd5976ac1404971b2693dcf7c2721e44
SLaguerta <slaguerta@alumni.nd.edu>
2016-10-10 18:55:30 -0500

    Added completed Course.java

--- java-track/assignments/Course.java
+++ java-track/assignments/Course.java
@@ -5,15 +5,17 @@ public class Course {
 	private int remainingSeats;
 	private Student[] studentRoster;
 	
-	public Course (String name, int credits, int remainingSeats, Student[] studentRoster){
-		this.name = name;
-		this.credits = credits;
-		this.remainingSeats = remainingSeats;
-		this.studentRoster = studentRoster;
-		
-	}
+//	public Course (String name, int credits, int remainingSeats, Student[] studentRoster){
+//		//Student[] studentRoster = {new Student()};
+//		this.name = name;
+//		this.credits = credits;
+//		this.remainingSeats = remainingSeats;
+//		this.studentRoster = new Student[remainingSeats];
+//		
+//	}
 	
 	public Course (String name, int credits, int remainingSeats){
+		this.studentRoster = new Student [remainingSeats];
 		this.name = name;
 		this.credits = credits;
 		this.remainingSeats = remainingSeats;
@@ -42,25 +44,37 @@ public class Course {
 	}
 	
 	public Boolean addStudent(Student newStudent){
+		
 		if(this.remainingSeats == 0){
 				return false;
 		}
-		for (int i = 0; i< studentRoster.length; i++){
-			if (studentRoster[i] != null && studentRoster[i].getName() == newStudent.getName()){
+		for (int i = 0; i< studentRoster.length; i++) {
+			if (studentRoster[i] instanceof Student && studentRoster[i].getName() == newStudent.getName()){
 				return false;
 			}
-		}
 		
+		}
 		studentRoster[studentRoster.length - this.remainingSeats] = newStudent;
 		remainingSeats--;
 		
 		return true;
+		
 	}
 
-
+	public double averageGPA(){
+		double sumOfGPA = 0.0;
+		int numberOfStudents = 0;
+		for(int i =0; i<studentRoster.length; i++){
+			if (studentRoster[i] instanceof Student){
+			sumOfGPA += studentRoster[i].getGPA();
+			numberOfStudents ++;
+			}
+		}
+		return sumOfGPA/numberOfStudents;
+	}
 	
 	public String toString(){
 		return "Name: " + this.name + " " + this.credits + ", " + this.remainingSeats;
 	}
 }
-	
+	//instanceof student
